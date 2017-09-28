e8466fa66f16b6243379f96bec735d5342bdbf6b
lsminter <lsminteris@gmail.com>
2017-09-28 11:51:54 -0700

    fixed bug

--- script.jsx
+++ script.jsx
@@ -113,7 +113,7 @@ class FilterLink extends Component {
   }
 }
 FilterLink.contextTypes = {
-  store: React.PropTypes.object
+  store: React.PropTypes
 };
 
 const Footer = () => (
@@ -190,7 +190,7 @@ const AddTodo = (props, { store }) => {
   );
 };
 AddTodo.contextTypes = {
-  store: React.PropTypes.object
+  store: React.PropTypes
 };
 
 const getVisibleTodos = (
@@ -247,7 +247,7 @@ class VisibleTodoList extends Component {
   }
 }
 VisibleTodoList.contextTypes = {
-  store: React.PropTypes.object
+  store: React.PropTypes
 };
 
 const TodoApp = () => (
